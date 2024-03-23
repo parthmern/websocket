e078e586d9a38289f993d44b67d28bed42490541
parthmern <parthmern@gmail.com>
2024-03-22 21:50:32 -0400

    client file added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,90 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useMemo, useState } from 'react'
+import {io} from "socket.io-client"
 
-function App() {
-  const [count, setCount] = useState(0)
+// if we are not going to use useMemo then we can put here
+//const socket = io("http://localhost:4000");
+
+export const App = () => {
+
+  const [text, setText] = useState();
+  const [socketId, setSocketId] = useState();
+  const [roomId, setRoomId] = useState();
+
+  //here rerendered everytime while we putting anything in input and 
+  // in input we are using usestate text value so because of usestate updation rerender of whole component happens
+  // but we need that socket is rendered only one time and it should be global variable so useMemo
+  
+  console.log("component rerednerd");
+  const socket = useMemo(()=>{
+    return io("http://localhost:4000")
+  }, [])
+  // in useEffct without state var we cannot assign value to variable of outside
+  // we can do it in useMemo
+
+  useEffect(()=>{
+    
+    console.log("useeff run");
+    socket.on("connect", ()=>{
+      console.log("connected with id", socket.id);
+      setSocketId(socket.id);
+    })
+
+    socket.on("emit", (data)=> {
+      console.log(data);
+    })
+
+    // receving emitted from server
+    socket.on("rcv-data", (data)=> {
+      console.log(data);
+    })
+
+    socket.on("roommsg",(data)=>{
+      console.log("secretmsg", data);
+
+    })
+
+    return ()=>{
+      socket.disconnect() ;
+    }
+
+  },[]);
+  
+  function handleSubmit(e){
+    console.log("called");
+    e.preventDefault();
+    
+    //socket.emit("msg", text);
+
+    const msg = {
+      room : roomId ,
+      data : text ,
+    }
+
+    socket.emit("secretmsg", msg);
+
+  }
+  
 
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        socket id {socketId}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <label>Room id =</label>
+      <input onChange={(e)=>{
+          setRoomId(e.target.value);
+        }} value={roomId}></input>
+      <form onSubmit={handleSubmit}>
+        <input onChange={(e)=>{
+          setText(e.target.value);
+        }} value={text}></input>
+        <button type='submit'>submit</button>
+      </form>
+
+        <div>
+          
+        </div>
+
+    </div>
   )
 }
-
-export default App
